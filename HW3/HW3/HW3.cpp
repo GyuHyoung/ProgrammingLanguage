ca931f2bcfa0ca7a8b699aa7f621adce872a6660
GyuHyoung <do0ob1124@gmail.com>
2020-05-17 00:28:55 +0900

    20200517

--- HW3/HW3/HW3.cpp
+++ HW3/HW3/HW3.cpp
@@ -4,12 +4,19 @@
 #include <vector>
 #include <stack>
 #include <malloc.h>
-
+#include <regex>
 using namespace std;
 
 vector<string> input;
 stack<string> st;
 vector<int> error_msg;
+vector<struct defun> defun_list;
+
+typedef struct defun {
+	string keyword;
+	vector<string> param;
+	vector<string> expression;
+}defun;
 
 typedef struct TreeNode {
 	string node;
@@ -36,6 +43,17 @@ bool isInt(string str) {
 		}
 	}
 	return ret;
+}
+
+int findKeyword(string str) {
+
+	for (int i = 0; i < defun_list.size(); i++) {
+		if (defun_list[i].keyword == str) {
+			return i;
+		}
+	}
+
+	return 777;
 }
 
 void error_check_num(string check) {
@@ -165,11 +183,62 @@ TreeNode* addNode(int start, int end) {
 
 }
 
+//IF, MINUS, (, ) 제외 다른게 있는지 확인. 있으면 치환. inputindex늘려줘야함
+void func(vector<string> input) {
+	for (int i = 0; i < input.size(); i++) {
+		if (input[i] == "(" || input[i] == ")") {
+			continue;
+		}
+		if (input[i] == "MINUS" || input[i] == "IF") {
+			continue;
+		}
+		if (isInt(input[i])) {
+			continue;
+		}
+		else {
+			//i는 ADD의 위치
+			int keyIdx = findKeyword(input[i]);
+			vector<pair<int, int>> param_list;
+
+			for (int j = i + 1; j < input.size(); j++) {
+				int opencnt = 0, closecnt = 0;
+				int startIdx = -1, endIdx = -1;
+
+				if (isInt(input[j]) == true) {
+					param_list.push_back(make_pair(j, j));
+				}
+				else {
+					if (input[j] == "(") {
+						if (startIdx == -1) {
+							startIdx = j;
+						}
+						opencnt++;
+					}
+					else if (input[j] == ")") {
+						closecnt++;
+
+						if (opencnt == closecnt) {
+							endIdx = j;
+							param_list.push_back(make_pair(startIdx, endIdx));
+							startIdx = -1;
+							endIdx = -1;
+						}
+					}
+				}
+			}
+			//치환하기(i-1부터 계산한 마지막 인덱스 + 1)
+			substitute();
+			//i 증가? j 증가?
+		}
+	}
+}
+
 void parse(char line[]) {
 	string tmp = "";
 	int opencnt = 0, closecnt = 0;
 
 	for (int i = 0; line[i]; i++) {
+
 		if (line[i] == '(') {
 			input.push_back("(");
 			opencnt++;
@@ -213,8 +282,11 @@ void operate(char line[]) {
 
 	input.clear();
 	error_msg.clear();
+	
 	//parsing
 	parse(line);
+
+	func(input);
 
 	//make tree
 	Root = addNode(0, input.size() - 1);
@@ -247,36 +319,14 @@ void operate(char line[]) {
 }
 
 void interpreter() {
+
 	ifstream ifile;
 	char filename[200];
 	char line[200];
 
-	cout << "파일 명을 입력하세요 >> ";
+	cout << "실행 파일 명을 입력하세요 >> ";
 	cin >> filename;
-	/*
-	수정 필요
-	!!
-	!!
-	*/
-	ifile.open(filename);
-
-	if (ifile.is_open()) {
-		cout << "파일 내용은 " << endl;
-		cout << "-------------------------" << endl;
-		while (ifile.getline(line, sizeof(line))) {
-			cout << line << endl;
-		}
-		cout << "-------------------------" << endl;
-		cout << "입니다." << endl << endl;
-
-	}
-	else {
-		cout << "파일 읽기 오류 " << endl;
-
-		return;
-	}
-
-	ifile.close();
+	
 	ifile.open(filename);
 
 	if (ifile.is_open()) {
@@ -284,6 +334,13 @@ void interpreter() {
 			operate(line);
 		}
 	}
+	else {
+		cout << "파일 읽기 오류 " << endl;
+
+		return;
+	}
+
+
 	cout << endl;
 	ifile.close();
 
@@ -292,11 +349,103 @@ void interpreter() {
 
 void printDefun() {
 	//defun list 출력
+	for (int i = 0; i < defun_list.size(); i++) {
+
+		cout << defun_list[i].keyword;
+
+		cout << " ( ";
+		for (int j = 0; j < defun_list[i].param.size(); j++) {
+			cout << defun_list[i].param[j] << " ";
+		}
+		cout << ") ";
+
+		for (int j = 0; j < defun_list[i].expression.size(); j++) {
+			cout << defun_list[i].expression[j] << " ";
+		}
+		cout << endl;
+	}
+
 
 }
 void defineDefun() {
 	//line으로 입력 받기
-	//엔터 누르면 정의된 defun list에 저장
+	string line;
+	defun* newDefun = new defun();
+
+	int a = getchar();
+	getline(cin, line);
+	
+	//엔터 누르면
+	//(구현) line이 길이 6보다 작을때
+	line = line.substr(6, line.size()-1);
+	string tmp;
+	int idx = 0;
+	
+	for (int i = 0; i < line.size(); i++) {
+		//keyword 저장
+		if (idx == 0 && line[i] == ' ') {
+			newDefun ->keyword = tmp;
+			tmp = "";
+			idx = i;	
+			continue;
+		}
+		
+		//매개변수 저장
+		//(구현) 매개변수 아무것도 없는지 검사
+		if (idx != 0 && line[i] == ')') {
+			string param = "";
+			for (int j = idx+2; j <= i; j++) {
+				if (line[j] == ' ' || line[j] == ')') {
+					if (param != "") {
+						newDefun->param.push_back(param);
+						param = "";
+					}
+				}
+				else {
+					param += line[j];
+				}
+			}
+			i += 2;
+			idx = i;
+			tmp = "";
+			break;
+		}
+		tmp += line[i];
+	}
+
+	//수식 저장
+	//(구현) 길이가 idx보다 길어야만 substr 가능
+	line = line.substr(idx, line.size() - 1);
+	int len = line.length() + 1;
+	char* cstr = new char[len];
+	//for (int i = 0; i < line.length(); i++) {
+	//	cstr[i] = line[i];
+	//}
+	strcpy(cstr, line.c_str());
+
+	parse(cstr);
+	for (int i = 0; i < input.size(); i++) {
+		newDefun->expression.push_back(input[i]);
+	}
+	input.clear();
+
+
+	//이미 있는 함수인지 검사
+	int error_exist = 0;
+	for (int i = 0; i < defun_list.size(); i++) {
+		if (newDefun->keyword == defun_list[i].keyword) {
+			cout << "이미 존재하는 함수 입니다." << endl;
+			error_exist = 1;
+			//(구현) 에러 "이미 존재하는 함수 입니다."
+		}
+	}
+
+	//에러 없으면
+	//정의된 defun list에 저장
+	if (error_exist == 0) {
+		defun_list.push_back(*newDefun);
+	}
+
 }
 
 void command() {
@@ -314,12 +463,12 @@ void command() {
 	switch (exe) {
 
 	case 1:
-		//define defun
+		//define defun(O)
 		defineDefun();
 		break;
 
 	case 2:
-		//print defun
+		//print defun(O)
 		printDefun();
 		break;
 
@@ -329,7 +478,7 @@ void command() {
 		break;
 
 	case 4:
-		//exit
+		//exit(O)
 		cout << "프로그램을 종료합니다." << endl;
 		exit(0);
 		break;
